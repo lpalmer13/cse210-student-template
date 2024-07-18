2501dc788ce730114e6ab26e8ff0af7e63abdb6a
lpalmer13 <pal20029@byui.edu>
2024-07-17 21:41:45 -0600

    Commit

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,14 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Address lectureAddress = new Address("123 Main St", "Anytown", "CA", "12345");
-        Event lecture = new Lecture("Tech Talk", "A lecture on the latest in tech", new DateTime(2024, 8, 12), "10:00 AM", lectureAddress, "Jane Doe", 100);
+        Address lectureAddress = new Address("123 Fart St", "Gasville", "FL", "12345");
+        Event lecture = new Lecture("Silent but Deadly", "A lecture on the art of stealth farts", new DateTime(2024, 8, 12), "10:00 AM", lectureAddress, "Jane Doe", 100);
 
-        Address receptionAddress = new Address("456 Elm St", "Othertown", "NY", "67890");
-        Event reception = new Reception("Networking Event", "An event to network with industry professionals", new DateTime(2024, 9, 5), "6:00PM", receptionAddress, "rsvp@example.com");
+        Address receptionAddress = new Address("456 Toot Ln", "Flatulence City", "NY", "67890");
+        Event reception = new Reception("Fart Festival", "A reception celebrating all things fart-related", new DateTime(2024, 9, 5), "6:00 PM", receptionAddress, "rsvp@fartfest.com");
 
-        Address outdoorAddress = new Address("789 Oak St", "Sometown", "TX", "54321");
-        Event outdoorGathering = new OutdoorGathering("Community Picnic", "A picnic for the community", new DateTime(2024, 7, 20), "1:00 PM", outdoorAddress, "Sunny and warm");
+        Address outdoorAddress = new Address("789 Gas Blvd", "Windy Town", "TX", "54321");
+        Event outdoorGathering = new OutdoorGathering("Outdoor Fartathon", "A gathering for outdoor fart competitions", new DateTime(2024, 7, 20), "1:00 PM", outdoorAddress, "Sunny with a chance of wind");
 
         Event[] events = { lecture, reception, outdoorGathering };
 
