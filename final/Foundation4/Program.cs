6e6fe8f2c18004416b5becb5e09b7ede0b97b76d
lpalmer13 <pal20029@byui.edu>
2024-07-20 23:34:27 -0600

    Commit

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -7,9 +7,12 @@ class Program
     {
         List<Activity> activities = new List<Activity>
         {
-            new Running(new DateTime(2023, 11, 3), 60, 6),
-            new Cycling(new DateTime(2023, 11, 4), 60, 15),
-            new Swimming(new DateTime(2023, 11, 5), 60, 30)
+            new Running(new DateTime(2023, 11, 4), 40, 3.0),
+            new Cycling(new DateTime(2023, 11, 5), 45, 15.0),
+            new Swimming(new DateTime(2023, 11, 6), 60, 30),
+            new Running(new DateTime(2023, 11, 11), 45, 5.0),
+            new Cycling(new DateTime(2023, 11, 12), 60, 18.0),
+            new Swimming(new DateTime(2023, 11, 13), 75, 40)
         };
 
         foreach (var activity in activities)
